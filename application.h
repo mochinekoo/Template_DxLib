d27feed106f7d66bb8403331db356b47d3886a8f
mochineko <mochinekoo0113@gmail.com>
2025-12-13 22:20:09 +0900

    [add] ウインドウが出るようになりました

---
+++ application.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Screen {
+	const int WIDTH = 1280;
+	const int HEIGHT = 720;
+	const int COLORBIT = 32;
+	const int BACKCOLOR[3] = {0, 0, 0};
+	const int FPS = 60;
+	const char* TITLE = "GameTemplate";
+}
